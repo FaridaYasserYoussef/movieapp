9c000adbeae195b951329cbea54fa9b97edabbab
YahyaEltayeeb <ym615318@gmail.com>
2024-12-16 04:36:43 +0200

    changButton

--- lib/home/view/widget/custom_button.dart
+++ lib/home/view/widget/custom_button.dart
@@ -13,42 +13,74 @@ bool iscleck = false;
 class _CustomButtonState extends State<CustomButton> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned(
-            bottom: -6,
-            right: -4,
-            child: IconButton(
-              icon: Icon(Icons.bookmark),
-              color:
-                  iscleck == false ? AppColor.opacityGrayColor : Colors.amber,
-              iconSize: 50,
-              onPressed: () {
-                setState(() {
-                  iscleck = !iscleck;
-                });
-              },
-            )),
-        IconButton(
+    return Positioned(
+      top: -14,
+      left: -19,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          IconButton(
+            icon: Icon(Icons.bookmark),
+            color: iscleck == false ? AppColor.opacityGrayColor : Colors.amber,
+            iconSize: 50,
             onPressed: () {
               setState(() {
                 iscleck = !iscleck;
               });
             },
-            icon: Stack(
-              children: [
-                Positioned(
-                  right: 9,
-                  bottom: 8,
-                  child: Icon(
-                    iscleck == false ? Icons.add : Icons.done_outlined,
-                    size: 20,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ))
-      ],
+          ),
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  iscleck = !iscleck;
+                });
+              },
+              icon: Icon(
+                iscleck == false ? Icons.add : Icons.done_outlined,
+                size: 30,
+                color: Colors.white,
+              ))
+        ],
+      ),
     );
   }
 }
+
+
+//  Stack(
+//       children: [
+//         Positioned(
+//             bottom: -6,
+//             right: -4,
+//             child: IconButton(
+//               icon: Icon(Icons.bookmark),
+//               color:
+//                   iscleck == false ? AppColor.opacityGrayColor : Colors.amber,
+//               iconSize: 50,
+//               onPressed: () {
+//                 // setState(() {
+//                 //   iscleck = !iscleck;
+//                 // });
+//               },
+//             )),
+//         IconButton(
+//             onPressed: () {
+//               setState(() {
+//                 iscleck = !iscleck;
+//               });
+//             },
+//             icon: Stack(
+//               children: [
+//                 Positioned(
+//                   right: 9,
+//                   bottom: 8,
+//                   child: Icon(
+//                     iscleck == false ? Icons.add : Icons.done_outlined,
+//                     size: 20,
+//                     color: Colors.white,
+//                   ),
+//                 ),
+//               ],
+//             ))
+//       ],
+//     );
